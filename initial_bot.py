a785be30b69bcf3c92fdf8bc63f175f8b0e3681c
Lewis0305 <lewiss4@oregonstate.edu>
2022-03-07 18:41:49 -0800

    Simple Bot Setup

--- initial_bot.py
+++ initial_bot.py
@@ -1,1 +1,23 @@
-pass
+import discord
+from discord.ext import commands
+
+import config
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.command()
+async def status(ctx):
+    await ctx.send('Pi Bot: ONLINE')
+
+
+@bot.command()
+async def chain(ctx):
+    await ctx.send('!one')
+
+
+@bot.command()  # DOESN'T WORK (not called by chain)
+async def one(ctx):
+    await ctx.send('!two')
+
+bot.run(config.Token)
