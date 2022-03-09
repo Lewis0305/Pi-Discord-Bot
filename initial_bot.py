a84903320e2156c7d083d9fd4372b889e1cd2230
Lewis0305 <lewiss4@oregonstate.edu>
2022-03-09 00:42:09 -0800

    Added Rating and Clip Use Button Functionality

--- initial_bot.py
+++ initial_bot.py
@@ -45,7 +45,6 @@ async def button(ctx):
         [Button(label="Delete", style=4, custom_id="button_delete")]
     ])
     interaction = await bot.wait_for("button_click")
-    print(interaction.component.label)
     try:
         await interaction.respond()
     except:
@@ -55,24 +54,70 @@ async def button(ctx):
 
 @bot.command()
 async def video_test(ctx):
-    clip_number = 4
     clips = twitch.get_clips(broadcaster_id="37402112", first=30, started_at=DT.datetime(2021, 12, 6))
 
-    print(clips)
-    input()
     for n in range(30):
         if clips["data"][n]["duration"] > 50:
-            await ctx.send("Title: **" + clips["data"][n]["title"] + "**\n" +
+            message = ("Title: **" + clips["data"][n]["title"] + "**\n" +
                            "Broadcaster: **" + clips["data"][n]["broadcaster_name"] + "**\n" +
                            clips["data"][n]["url"])
         else:
-            await ctx.send("Title: **" + clips["data"][n]["title"] + "**\n" +
+            message = ("Title: **" + clips["data"][n]["title"] + "**\n" +
                            "Broadcaster: **" + clips["data"][n]["broadcaster_name"] + "**\n" +
                            clips["data"][n]["thumbnail_url"].split("-preview")[0] + ".mp4")
-        input()
 
-    #print(clips["data"][clip_number])
-    #await ctx.send("title\n" + clips["data"][clip_number]["thumbnail_url"].split("-preview")[0] + ".mp4")
-    #await ctx.send(clips["data"][clip_number]["url"])
+        # RATING ##########################################################################################
+        await ctx.send(message, components=[
+            [Button(label="1", style=2, custom_id="1"),
+             Button(label="2", style=2, custom_id="2"),
+             Button(label="3", style=2, custom_id="3"),
+             Button(label="4", style=2, custom_id="4"),
+             Button(label="5", style=2, custom_id="5")],
+            [Button(label="6", style=2, custom_id="6"),
+             Button(label="7", style=2, custom_id="7"),
+             Button(label="8", style=2, custom_id="8"),
+             Button(label="9", style=2, custom_id="9"),
+             Button(label="10", style=2, custom_id="10")],
+            [Button(label="Delete", style=4, custom_id="button_delete")]
+        ])
+        interaction_rate = await bot.wait_for("button_click")
+        try:
+            await interaction_rate.respond()
+        except:
+            pass
+
+        await ctx.send("Rated: " + interaction_rate.component.label)
+        print(interaction_rate.component.label)
+
+        # USE AS CLIP ####################################################################################
+        await ctx.send("Use as a **clip**?", components=[
+            [Button(label="Yes", style=3, custom_id="yes"),
+             Button(label="No", style=4, custom_id="no")]
+        ])
+        interaction_clip = await bot.wait_for("button_click")
+        try:
+            await interaction_clip.respond()
+        except:
+            pass
+
+        await ctx.send("Rated: " + interaction_clip.component.label)
+        print(interaction_clip.component.label)
+
+
+        await ctx.send("Done\n\n\n\n\n\nNext")
+
+
+    # clip_number = 4
+    # print(clips["data"][clip_number])
+    # await ctx.send("title\n" + clips["data"][clip_number]["thumbnail_url"].split("-preview")[0] + ".mp4")
+    # await ctx.send(clips["data"][clip_number]["url"])
+
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send("Say hello!")
+
+    msg = await bot.wait_for("message")
+    await ctx.send(f"Hello {msg.author}! ({msg.content})")
 
 bot.run(config.DISCORD_TOKEN)
