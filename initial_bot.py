ffe1c19b2fc81606b4738a8edc866ac08e1161cd
Lewis0305 <lewiss4@oregonstate.edu>
2022-03-21 02:50:09 -0700

    Removed Scrape Code

--- initial_bot.py
+++ initial_bot.py
@@ -99,6 +99,7 @@ async def video_test(ctx):
         await rate_message.edit(components=[])
         await ctx.send(f"*Rated: **{interaction_rate.component.label}***. By {interaction_rate.author}")
         if interaction_rate.component.label == "delete":
+            # TODO Add 'Are You Sure?' to delete
             continue
         clip_rating = RateInfo(interaction_rate.component.label)
 
@@ -161,36 +162,6 @@ async def video_test(ctx):
         scrape_database.to_csv(config.SCRAPE_CSV)
 
 
-@bot.command()  # This will be a normal function
-async def scrape_videos(ctx):
-    # TODO Intelligent Video Scraping Process (BASED ON MY YT CHANNELS)
-
-    scrape_database = pd.read_csv(config.SCRAPE_CSV, index_col=0)
-    archive_database = pd.read_csv(config.ARCHIVE_CSV, index_col=0)
-
-    month_ago = dt.datetime.now() - dt.timedelta(days=30)
-    clips = twitch.get_clips(broadcaster_id="94753024", first=10, started_at=month_ago)
-
-    for clip in clips["data"]:
-        if clip["id"] not in archive_database.index:
-            scrape_database.loc[clip["id"]] = [clip["broadcaster_id"],
-                                               clip["game_id"],
-                                               clip["created_at"],
-                                               clip["view_count"]]
-            archive_database.loc[clip["id"]] = [clip["url"]]
-        else:
-            pass  # print(clip['id'] + ": Duplicate")
-
-    scrape_database.to_csv(config.SCRAPE_CSV)
-    archive_database.to_csv(config.ARCHIVE_CSV)
-
-    # No TO DO Automate: Dataframe for each channel? (I WILL PROBABLY MAKE THIS)
-
-
-    # TODO Four programs: discord bot, twitch trend/activity monitor, Server, Client
-    pass
-
-
 @bot.command()
 async def video_rated(ctx, *args):
     video_database = pd.read_csv(config.VIDEO_CSV, index_col=0)
@@ -212,27 +183,6 @@ async def video_rated(ctx, *args):
     else:
         await ctx.send(f"No videos rated {args[0]}")
 
-
-def twitch_vid_test():
-
-    # Adds any new games that have reached the current top 10
-    game_database = pd.read_csv(config.GAMES_CSV, index_col=0)
-    games = twitch.get_top_games(first=10)  # sorted by current active viewers
-    for n in games["data"]:
-        game = n
-        if int(game["id"]) not in game_database.index:
-            game_database.loc[game["id"]] = [game["name"]]
-    game_database.to_csv(config.GAMES_CSV)
-
-    # Adds any new casters that have reached the current top 15 clips
-    caster_database = pd.read_csv(config.BROADCASTER_CSV, index_col=0)
-    month_ago = dt.datetime.now() - dt.timedelta(days=30)
-    clip_by_id = twitch.get_clips(game_id="509658", first=15, started_at=month_ago)
-    for clip in clip_by_id["data"]:
-        if int(clip["broadcaster_id"]) not in caster_database.index:
-            print(clip["broadcaster_id"] + "," + clip["broadcaster_name"])
-            # caster_database.loc[clip["broadcaster_id"]] = [clip["broadcaster_name"]]
-    caster_database.to_csv(config.BROADCASTER_CSV)
 
 # TO DO Function to fix outdated clip info (as needed?)
 
